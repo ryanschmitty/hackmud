3fba4cc73a4c6a8e46ab540dc3645da385571052
Ryan Schmitt <rschmitt@Ryans-MacBook-Pro.local>
2017-01-19 16:07:37 -0800

    Finalized harvest

--- harvest.js
+++ harvest.js
@@ -1,26 +1,38 @@
-function(context, args)
+function(c, a)
 {
     var D=#s.dtr.lib();  
-    var result = []
-    if (!args) {
+
+    if (!a) {
+        var r = []
         var math = #s.math.t1({})
         for (var i in math) {
-            if (/\S/.test(math[i])) {
-                result.push("harvest{t:"+/#s\..+?(?=})/.exec(D.decolor(math[i]))[0]+"}")
-            }
+            var loc = /#s\..+?(?=})/.exec(D.decolor(math[i]))
+            if (loc)
+                r.push("harvest{t:"+loc[0]+"}")
+        }
+        return r
+    }
+
+    if (a.t) {
+        var p = #s.palad1n.harvest(a)
+        if (p.ok) {
+            var r = []
+            for (var i in p.l)
+                r.push(/#s\..+?(?=})/.exec(D.decolor(p.l[i]))[0].trim())
+            #db.r({rs:{$exists:true}})
+            #db.i({rs:r})
+            return "harvest{n:true}"
         }
     }
-    else {
-        var pal = #s.palad1n.harvest(args)
-        if (pal.ok) {
-            for (var i in pal.l) {
-                if (i >= 15) break;
-                result.push("math.t1{t1:"+/#s\..+?(?=})/.exec(D.decolor(pal.l[i]))[0]+"}")
-            }
-        }
-        else {
-            result.push(pal)
-        }
+
+    if (a.n) {
+        var rs = #db.f({rs:{$exists:true}}).first().rs
+        var s = "math.t1{t1:"+rs[0]+"}\n" +
+                "math.t1{t1:"+rs[1]+"}\n" +
+                "math.t1{t1:"+rs[2]+"}\n"
+        #db.u({rs:{$exists:true}}, {$pop:{rs:-1}})
+        #db.u({rs:{$exists:true}}, {$pop:{rs:-1}})
+        #db.u({rs:{$exists:true}}, {$pop:{rs:-1}})
+        return s
     }
-    return result
 }
